909d3f1d6f42366630b1bb3c8a04d665444ef004
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-09-23 09:23:42 +0200

    fix: navbar overlaped calendar

--- src/navbarBootstrap.js
+++ src/navbarBootstrap.js
@@ -1,7 +1,7 @@
 import { createNavbar } from './components/navbar/Navbar.js';
 
 const { mainComponent, internalDomElements, renderDisplays } = createNavbar();
-document.body.appendChild(mainComponent);
+document.body.insertBefore(mainComponent, document.getElementById('calendar-main'));
 
 export const navbarDomElements = internalDomElements;
 export const navbarRenderDisplays = renderDisplays;
